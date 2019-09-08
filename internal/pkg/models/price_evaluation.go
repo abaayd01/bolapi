a983334c0d74565fac43d46bad410588a7e548bf
Ayden Aba <ayden@codefishstudio.com>
2019-09-08 11:16:01 +0930

    setup price_evaluation model and repo for insert

--- internal/pkg/models/price_evaluation.go
+++ internal/pkg/models/price_evaluation.go
@@ -1,1 +1,23 @@
 package models
+
+import (
+	"bolapi/internal/pkg/bolproto"
+	"time"
+)
+
+type PriceEvaluation struct {
+	bolproto.PriceEvaluationResponse
+	CreatedTime time.Time
+}
+
+func New(priceEvaluationResponse *bolproto.PriceEvaluationResponse) (*PriceEvaluation, error) {
+	priceEvaluation := PriceEvaluation{
+		CreatedTime: time.Now(),
+	}
+
+	priceEvaluation.PriceEvaluationResponse = *priceEvaluationResponse
+
+	return &priceEvaluation, nil
+}
+
+type PriceEvaluations []PriceEvaluation
--- internal/pkg/repositories/price_evaluation.go
+++ internal/pkg/repositories/price_evaluation.go
@@ -1,1 +1,33 @@
 package repositories
+
+import (
+	"bolapi/internal/pkg/database"
+	"bolapi/internal/pkg/models"
+	"log"
+)
+
+type PriceEvaluationRepository interface {
+	Insert(priceEvaluation *models.PriceEvaluation) error
+}
+
+type priceEvaluationRepository struct {
+	DB database.DBInterface
+}
+
+func NewPriceEvaluationRepository(DB database.DBInterface) *priceEvaluationRepository {
+	return &priceEvaluationRepository{DB: DB}
+}
+
+func (repo *priceEvaluationRepository) Insert(priceEvaluation *models.PriceEvaluation) error {
+	query := `
+	`
+
+	_, err := repo.DB.Exec(query)
+
+	if err != nil {
+		log.Printf("Error inserting PriceEvaluation")
+		return err
+	}
+
+	return nil
+}
